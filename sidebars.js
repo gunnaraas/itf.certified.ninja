773c0568561356ff1891eb5fba14e6d99d10ad9b
Gunnar Aas <gunnar.aas218@gmail.com>
2020-12-14 20:12:44 -0600

    sidebar update

--- sidebars.js
+++ sidebars.js
@@ -2,7 +2,7 @@ module.exports = {
   someSidebar: {
     'Introduction': ['ch0-gettingstarted'],
     'Computer Hardware': ['ch01-components', 'ch02-peripherals', 'ch03-devices'],
-    'Computer Software': ['ch04-operatingsystems', 'ch05-software', 'ch06-softwaredev'],
-    'IT Infrastructure': ['ch07-databases', 'ch08-networking', 'ch09-securityconcepts', 'ch10-securitypractices', 'ch11-businesscontinuity']
+    'Computer Software': ['ch04-operatingsystems', 'ch05-software', 'ch06-softwaredev', 'ch07-databases'],
+    'IT Infrastructure': ['ch08-networking', 'ch09-securityconcepts', 'ch10-securitypractices', 'ch11-businesscontinuity']
   },
 };
